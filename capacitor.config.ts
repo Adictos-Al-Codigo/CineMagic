4fc9f810255f2803125b3591a317acd6c447e32c
Celorio2001 <jahir.celorio2001@gmail.com>
2023-06-06 09:51:59 -0500

    Estrctura de Ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'prueba5-consumo_api',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
